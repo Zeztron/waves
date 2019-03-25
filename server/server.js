53aeffe3129260463523960785e3ced423d23ede
Zeztron <pateltharsh@gmail.com>
2019-03-25 12:44:53 -0400

    set up the client side

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ const { auth } = require("./middleware/auth");
 const { admin } = require('./middleware/admin');
 
 
+// Enable cross domain script
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
+
 // ====================
 //        PRODUCTS
 // ====================
