a6d97dd392bfb089ccc54ae7aa9f78e6c3726019
Zeztron <pateltharsh@gmail.com>
2019-03-25 09:36:55 -0400

    created logout route

--- server/server.js
+++ server/server.js
@@ -74,7 +74,25 @@ app.post('/api/users/login', (req, res) => {
     });
 });
 
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        { token: '' },
+        (err, doc) => {
+            if(err) return res.json({
+                success: false,
+                err
+            });
+            return res.status(200).send({
+                success: true
+            });
+        }
+    );
+});
 
+
+
+// Fire up the server
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
