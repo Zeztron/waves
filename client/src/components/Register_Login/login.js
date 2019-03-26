4aa24299b6e9252591ea9163c3e2d168c8993c24
Zeztron <pateltharsh@gmail.com>
2019-03-26 13:02:04 -0400

    added onchange for signup form and error messages

---
+++ client/src/components/Register_Login/login.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { update } from '../utils/Form/formActions';
+import FormField from '../utils/Form/FormField';
+
+class Login extends Component {
+
+    state = {
+        formError: false,
+        formSuccess: '',
+        formData: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage: ''
+            },
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'password_input',
+                    type: 'password',
+                    placeholder: 'Enter your password'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage: ''
+            }
+        }
+    }
+
+    updateForm = (element) => {
+        const newFormData = update(element, this.state.formData, 'login');
+        this.setState({
+            formError: false,
+            formData: newFormData
+        })
+    }
+
+    submitForm = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="signin_wrapper">
+                <form onSubmit={(event) => this.submitForm(event)}>
+                    <FormField
+                        id={'email'}
+                        formData={this.state.formData.email}
+                        change={(element) => this.updateForm(element)}
+                    />
+                    <FormField
+                        id={'password'}
+                        formData={this.state.formData.password}
+                        change={(element) => this.updateForm(element)}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect()(Login);
