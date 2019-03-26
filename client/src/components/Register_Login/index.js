f61707802eefec5e398ab3a035def1323cf4110e
Zeztron <pateltharsh@gmail.com>
2019-03-26 11:49:58 -0400

    created register component

---
+++ client/src/components/Register_Login/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MyButton from '../utils/button';
+
+const RegisterLogin = () => {
+    return (
+        <div className="page_wrapper">
+            <div className="container">
+                <div className="register_login_container">
+                    <div className="left">
+                        <h1>New Customers</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore molestias fuga quia nisi ea, porro suscipit, eius deleniti doloremque inventore est omnis eaque, earum quibusdam odio sunt cupiditate numquam similique.</p>
+                        <MyButton
+                            type="default"
+                            title="Create an account"
+                            linkTo="/register"
+                            addStyles={{
+                                margin: '10px 0 0 0'
+                            }}
+                        />
+                    </div>
+                    <div className="right">
+                        <h2>Registered Customers</h2>
+                        <p>If you have an account, please log in.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterLogin;
