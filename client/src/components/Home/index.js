146c8ba192b9c86a3abcbbca1b9b5668e9a55693
Zeztron <pateltharsh@gmail.com>
2019-03-28 11:41:44 -0400

    added slider and promotion to home page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import HomeSlider from './HomeSlider';
+import HomePromotion from './HomePromotion';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                home
+                <HomeSlider/>
+                <HomePromotion/>
             </div>
         )
     }
