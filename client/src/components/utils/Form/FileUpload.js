3dcc7df08cd0ccf654354e77fbeed64313b65027
Zeztron <pateltharsh@gmail.com>
2019-04-13 15:33:04 -0400

    can now add image to add product

--- client/src/components/utils/Form/FileUpload.js
+++ client/src/components/utils/Form/FileUpload.js
@@ -39,7 +39,18 @@ export default class FileUpload extends Component {
     }
 
     onRemove = (id) => {
+        axios.get(`/api/users/removeimage?public_id=${id}`)
+            .then(response => {
+                let images = this.state.uploadedFiles.filter((item) => {
+                    return item.public_id !== id
+                });
 
+                this.setState({
+                    uploadedFiles: images
+                }, () => {
+                    this.props.imagesHandler(images);
+                });
+            });
     }
 
     showUploadedImages = () => (
@@ -53,6 +64,15 @@ export default class FileUpload extends Component {
             </div>
         ))
     )
+
+    static getDerivedStateFromProps(props, state) {
+        if(props.reset) {
+            return state = {
+                uploadedFiles: []
+            }
+        }
+        return null;
+    }
     
 
     render() {
--- client/src/components/utils/Form/formActions.js
+++ client/src/components/utils/Form/formActions.js
@@ -87,7 +87,11 @@ export const resetFields = (formData, formName) => {
     }
 
     for(let key in newFormData) {
-        newFormData[key].value = '';
+        if(key === 'images') {
+            newFormData[key].value = [];
+        } else {
+            newFormData[key].value = '';            
+        }
         newFormData[key].valid = false;
         newFormData[key].touched = false;
         newFormData[key].validationMessage = '';
